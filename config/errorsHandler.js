717eee267bcb8392a4688ce4ad06d1ab879ab522
Shereen Haj Mahmoud <62863339+sherehar@users.noreply.github.com>
2021-11-12 17:12:09 +0100

    Update errorsHandler.js

--- config/errorsHandler.js
+++ config/errorsHandler.js
@@ -6,5 +6,24 @@ const handleErrors = (err) => {
     })
     return errors
 }
-
-module.exports = {handleErrors};
+const handleUserErrors = (err) => {
+    
+    let errors = {username : '' , email : '' , password : ''}
+    if(err.message === 'incorrect Email'){
+    errors.email = 'That email is not registered'
+    }
+    if(err.message === 'incorrect password'){
+    errors.password = 'That password is not correct'
+    }
+    if(err.code === 11000){
+    errors.email = 'That email is already registered'
+    return errors;
+    }
+    if(err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach( error => {
+        errors[error.properties.path] = error.properties.message;
+    })
+    }
+    return errors
+}
+module.exports = {handleErrors,handleUserErrors};
